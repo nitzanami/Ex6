e5ab0422f38959953b60c15c0d8212b78b9c171c
neriya333 <neriya333@cs.huji.ac.il>
2023-01-16 15:24:15 +0200

    added main to project. missing passing over the input twice

---
+++ main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.function.Predicate;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        if(args.length != 2) throw new InputMismatchException("Wrong number of parameters");
+        try(BufferedReader br = new BufferedReader(new FileReader(args[1]))) {
+            String line;
+            LineProcessor p = new LineProcessor();
+            while ((line = br.readLine()) != null) {
+                p.procesLine(line);
+            }
+            
+        } catch (IOException e) { // todo check which exception is needed to be thrown away.
+            e.printStackTrace();
+        }
+    }
+}
