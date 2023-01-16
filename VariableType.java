c1a6e4c09a4e9ca3c58fcb40c3ec717cecc74a97
nitzanami <nitzan.amihai1@gmail.com>
2023-01-16 15:34:17 +0200

    fixed packages

--- VariableType.java
+++
@@ -1,3 +0,0 @@
-public enum VariableType {
-    INT,DOUBLE,CHAR,BOOLEAN,STRING
-}
---
+++ s_java/symbol_managment/FunctionManager.java
@@ -0,0 +1,18 @@
+package s_java.symbol_managment;
+
+import s_java.variable.*;
+
+import java.util.List;
+
+public class FunctionManager {
+
+    public List<VariableType> getParameterTypes(){
+        return null;
+    }
+    public boolean doesFunctionExist(String functionName){
+        return false;
+    }
+    public void addFunction(String functionName, List<VariableType> parameterTypes){
+
+    }
+}
